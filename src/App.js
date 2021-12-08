b7a764b7053d7678bf907199f4a6d4e5cfa8a9eb
Anil K. Panta <anilpanta2053@gmail.com>
2021-12-08 12:39:55 +0545

    global styled component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
 import { Container } from "./components/style/Container.style";
+import { GlobalStyles } from "./components/style/Global";
 
 const theme = {
   colors: {
@@ -13,6 +14,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <>
+        <GlobalStyles />
         <Header />
         <Container>
           <h1>Hello</h1>
---
+++ src/components/style/Global.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+body{
+    background: #494949;
+    font-family: 'Poppins', sans-serif;
+}
+`;
